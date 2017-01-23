c0274bf5f502e767b126bca6fd6f472ad88b950e
Sofia Lunkes <sofia.big@live.com>
2017-01-23 18:41:34 -0200

    route for heartbeat without put and get without patient

---
+++ routes/heartbeats.js
@@ -0,0 +1,43 @@
+module.exports = app =>{
+	const Heartbeats = app.db.models.Heartbeats;
+
+	app.route("/heartbeats/:id")
+	.all(app.auth.authenticate())
+	.get((req, res) => {
+		Heartbeats.findOne({
+			where : {
+				patient_id : req.patient.id
+			}})
+		.then((result) => {
+			if(result){
+				res.json(result)
+			}else{
+				res.sendStatus(404);
+			}
+		})
+		.catch(error => {
+			res.status(412).json({msg: error.message});
+		});
+	})
+
+  .post((req, res) => {
+    req.body.patient_id = req.patient.id;
+    Heartbeats.create(req.body)
+      .then((result) => {res.json(result)})
+      .catch((error) => {
+        res.status(412).json({msg: error.message});
+      });
+  })
+
+	.delete((req, res) => {
+		Heartbeats.destroy({
+			where : {
+				id : req.params.id ,
+				patient_id : req.patient.id
+			}})
+		.then(result => res.sendStatus(204))
+		.catch(error => {
+			res.status(412).json({msg : error.message});
+		});
+	});
+};
