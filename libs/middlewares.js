55dc7000023dff4125a29cc9062062097a6f3d48
Jonatas Leon <jonatas.leon@gmail.com>
2017-02-03 02:52:13 -0200

    Change name of the dir of static files

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
 
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
-  app.use(express.static(path.join(__dirname, 'app_client')));
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use((req, res, next) => {
     delete req.body.id;
     next();
